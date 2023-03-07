c607e286e9c18e1a16a3a78dea9ca26283168412
sixletters <e0406779@u.nus.edu>
2023-03-08 01:20:38 +0800

    updates

--- go.mod
+++ go.mod
@@ -8,4 +8,9 @@ require (
 	github.com/robertkrimen/otto v0.2.1
 )
 
-require gopkg.in/sourcemap.v1 v1.0.5 // indirect
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr v1.4.10
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230305170008-8188dc5388df // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -3,26 +3,63 @@ package main
 import (
 	// "github.com/davecgh/go-spew/spew"
 	// "github.com/robertkrimen/otto/parser"
+	// "cs4215/goophy/parser"
+	"cs4215/goophy/parser"
+	"fmt"
+	"os"
+
+	antlr "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	// "github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	filename := "" // A filename is optional
-	src := `
-    // Sample xyzzy example
-    (function(){
-        if (3.14159 > 0) {
-            console.log("Hello, World.");
-            return;
-        }
+	// 	filename := "" // A filename is optional
+	// 	src := `
+	//     // Sample xyzzy example
+	//     (function(){
+	//         if (3.14159 > 0) {
+	//             console.log("Hello, World.");
+	//             return;
+	//         }
 
-        var xyzzy = NaN;
-        console.log("Nothing happens.");
-        return xyzzy;
-    })();
-`
-
+	//         var xyzzy = NaN;
+	//         console.log("Nothing happens.");
+	//         return xyzzy;
+	//     })();
+	// `
 	// Parse some JavaScript, yielding a *ast.Program and/or an ErrorList
-    parser  = JavaScriptParserBase
-	program, _ := parser.ParseFile(nil, filename, src, 0)
-	spew.Dump(program)
+	// parser = JavaScriptParserBase
+	// program, _ := parser.ParseFile(nil, filename, src, 0)
+	// spew.Dump(program)
+	Parse("1 + 1;2+3")
+}
+
+type TreeShapeListener struct {
+	*parser.BaseGrammarListener
+}
+
+func NewTreeShapeListener() *TreeShapeListener {
+	return new(TreeShapeListener)
+}
+
+// func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+// 	fmt.Println(ctx.GetText())
+// 	// fmt.Println("CHILD COUNT IS HERE")
+// 	// fmt.Println(ctx.GetChildCount())
+// }
+
+func (s *TreeShapeListener) VisitTerminal(node antlr.TerminalNode) {
+	fmt.Println(node.GetText())
+	// fmt.Println("CHILD COUNT IS HERE")
+	// fmt.Println(ctx.GetChildCount())
+}
+
+func Parse(program string) {
+	input, _ := antlr.NewFileStream(os.Args[1])
+	lexer := parser.NewGrammarLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
+	p := parser.NewGrammarParser(stream)
+	p.BuildParseTrees = true
+	// tree := p.
+	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), p.Prog())
 }
