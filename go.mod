1755f0906c2ec9a460f368f30e256e28cd2008ff
sixletters <e0406779@u.nus.edu>
2023-03-26 23:19:33 +0800

    merge

--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230305170008-8188dc5388df // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	k8s.io/utils v0.0.0-20230313181309-38a27ef9d749
 )
--- main.go
+++ main.go
@@ -5,81 +5,14 @@ import (
 	"cs4215/goophy/pkg/lexer"
 	"cs4215/goophy/pkg/machine"
 	"cs4215/goophy/pkg/parser"
+	"cs4215/goophy/pkg/repl"
 	"fmt"
+	"os"
 	"os/user"
 )
 
-// "cs4215/goophy/parser"
-
-// "github.com/davecgh/go-spew/spew"
-
-// func main() {
-// 	filename := "" // A filename is optional
-// 	src := `
-// 	    // Sample xyzzy example
-// 	    (function(){
-// 	        if (3.14159 > 0) {
-// 	            console.log("Hello, World.");
-// 	            return;
-// 	        }
-
-// 	        var xyzzy = NaN;
-// 	        console.log("Nothing happens.");
-// 	        return xyzzy;
-// 	    })();
-// 	`
-// 	// Parse some JavaScript, yielding a *ast.Program and/or an ErrorList
-// 	// parser = JavaScriptParserBase
-// 	node, _ := testparser.ParseFile(nil, "", `if (abc > 1) {}`, 0)
-// 	node.
-// 	// spew.Dump(node)
-// 	fmt.Println(node.Comments)
-// 	for _, f := range node.DeclarationList {
-// 		fn, ok := f.(*ast.FunctionDeclaration)
-// 		if !ok {
-// 			fmt.Println("SKIPS")
-// 			continue
-// 		}
-// 		fmt.Println(fn.Function.Name.Name)
-// 	}
-// 	// Parse("1 + 1;2+3")
-// }
-
-// type TreeShapeListener struct {
-// 	*parser.BaseGrammarListener
-// }
-
-// func NewTreeShapeListener() *TreeShapeListener {
-// 	return new(TreeShapeListener)
-// }
-
-// func (s *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-// 	fmt.Println(ctx.)
-// 	ctx.
-// 	// fmt.Println("CHILD COUNT IS HERE")
-// 	// fmt.Println(ctx.GetChildCount())
-// }
-
-// func (s *TreeShapeListener) VisitTerminal(node antlr.TerminalNode) {
-// 	fmt.Println(node.GetText())
-// 	fmt.Println(node.GetPayload())
-// 	fmt.Println(node.)
-// 	// fmt.Println("CHILD COUNT IS HERE")
-// 	// fmt.Println(ctx.GetChildCount())
-// }
-
-// func Parse(program string) {
-// 	input, _ := antlr.NewFileStream(os.Args[1])
-// 	lexer := parser.NewGrammarLexer(input)
-// 	stream := antlr.NewCommonTokenStream(lexer, 0)
-// 	p := parser.NewGrammarParser(stream)
-// 	parser.
-// 		p.BuildParseTrees = true
-// 	// tree := p.
-// 	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), p.Prog())
-// }
-
 func main() {
+
 	_, err := user.Current()
 	if err != nil {
 		panic(err)
@@ -92,6 +25,7 @@ func main() {
 	p := parser.New(l)
 	program := p.ParseProgram()
 	fmt.Println(program.String())
+<<<<<<< Updated upstream
 	fmt.Println(compiler.Compile(*program))
 	instrs := compiler.Compile(*program)
 	val := machine.Run(instrs)
@@ -103,4 +37,8 @@ func main() {
 	// fmt.Println(program.String())
 	// fmt.Printf("Feel free to type in commands\n")
 	// repl.Start(os.Stdin, os.Stdout)
+=======
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+>>>>>>> Stashed changes
 }
