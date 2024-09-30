f8feaaa51a944ca9f94621b16aae11ae57a13cc5
murtubak <murtubakchickenss@gmail.com>
2024-09-30 20:48:35 +0800

    Fix deprecated syscall/js calls

--- pkg/assets/script.js
+++ pkg/assets/script.js
@@ -3,4 +3,14 @@ const goWasm = new Go()
 WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
     .then((result) => {
         goWasm.run(result.instance)
-        })
+        
+        const input = "some code to compile";
+        const encoded = new TextEncoder().encode(input);
+        const len = encoded.length;
+        const ptr = instance.exports.malloc(len); // Use WebAssembly memory management to allocate memory
+        new Uint8Array(memory.buffer, ptr, len).set(encoded);
+    
+        const resPtr = instance.exports.WasmRunGo(ptr, len); // Call the Wasm function
+        const output = new TextDecoder().decode(new Uint8Array(memory.buffer, resPtr));
+        console.log(output); // Output result from the Go program
+    });
